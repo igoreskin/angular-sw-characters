5fb3ec50bc20b8ed498e3f2f9a09d5cff418b1c4
Igor Eskin <eskinhd@gmail.com>
2019-05-12 00:31:19 -0400

    Routing to launch component, link styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { CharacterComponent } from  './character/character.component';
-import { AppComponent } from './app.component';
+import { LaunchComponent } from './launch/launch.component';
 
 const routes: Routes = [
-  // { path: '', component: AppComponent },
+  { path: '', component: LaunchComponent },
   { path: ':id', component: CharacterComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { CharacterComponent } from './character/character.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LaunchComponent } from './launch/launch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CharacterComponent
+    CharacterComponent,
+    LaunchComponent
   ],
   imports: [
     BrowserModule,
