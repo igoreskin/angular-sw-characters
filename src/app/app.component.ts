99fa3402e28404666b6de9cbe643bba20f1ce312
Igor Eskin <eskinhd@gmail.com>
2019-05-23 16:53:41 -0400

    Soundtrack added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Howl } from 'howler';
 
 import { Character } from './character/character.model';
 import { Subscription } from 'rxjs';
@@ -21,12 +22,15 @@ export class AppComponent implements OnInit, OnDestroy {
 
   constructor(private characterService: CharacterService){}
 
+  sound = new Howl({src: ['../assets/Battles_Endor_Fight_Theme_Loop.mp3']})
+
   ngOnInit() {
     this.charSub = this.characterService.getCharacters()
     .subscribe(res => {
       this.characters = res['characters'];
       console.log(res['characters'])
     });
+    this.sound.play();
   }
 
   ngOnDestroy() {
